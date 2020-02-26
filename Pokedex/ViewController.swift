af342e90fca5fe923dd5415c54f77fbb9c5bc72f
Davide Tarantino <davide@MacBook-Pro-di-Davide.local>
2020-02-26 17:51:06 +0100

    first commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Davide Tarantino on 26/02/2020.
+//  Copyright © 2020 Davide Tarantino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
